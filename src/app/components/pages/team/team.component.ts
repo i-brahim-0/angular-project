037f5117f5fa77a9015c049f0965a73ac5042af2
Ibrahim Mahmoud <him9100@gmail.com>
2025-01-27 20:49:25 +0200

    add SCSS variables for main color and update styles across components

--- src/app/components/pages/team/team.component.ts
+++ src/app/components/pages/team/team.component.ts
@@ -3,8 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-team',
   templateUrl: './team.component.html',
-  styleUrls: ['./team.component.scss']
+  styleUrls: ['./team.component.scss'],
 })
 export class TeamComponent {
-
+  teams = [
+    {
+      img: 'assets/images/t1.jpg',
+      name: 'Fletch Skinner',
+      about: 'Water Color Painter',
+    },
+    {
+      img: 'assets/images/t2.jpg',
+      name: 'Niles Peppertrout',
+      about: 'House Painter',
+    },
+    {
+      img: 'assets/images/t3.jpg',
+      name: 'Benjamin Evalent',
+      about: 'Wood Painter',
+    },
+    {
+      img: 'assets/images/t4.jpg',
+      name: 'Gunther Beard',
+      about: 'Color Painter',
+    },
+  ];
 }
